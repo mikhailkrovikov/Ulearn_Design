39978741d726debb363714027e3dc86385c80f79
mkrovikov <mikhailkrovikov@gmail.com>
2026-07-30 19:17:56 +0300

    enterprise task

---
+++ Module_1_Incapsulation/Incapsulation.EnterpriseTask/Enterprise.cs
@@ -0,0 +1,42 @@
+﻿namespace Incapsulation.EnterpriseTask;
+
+public class Enterprise
+{
+    public readonly Guid Guid;
+
+    public string Name { get; set; }
+
+
+    private string _inn = "";
+    public string Inn
+    {
+        get => _inn;
+        set
+        {
+            if (_inn.Length != 10 && !_inn.All(char.IsDigit))
+                throw new ArgumentException();
+            else _inn = value;
+        }
+    }
+
+    public DateTime EstablishDate { get; set; }
+
+    public TimeSpan ActiveTimeSpan
+    {
+        get => DateTime.Now - EstablishDate;
+    }
+
+    public Enterprise(Guid guid)
+    {
+        Guid = guid;
+    }
+
+    public double GetTotalTransactionsAmount()
+    {
+        DataBase.OpenConnection();
+        var amount = 0.0;
+        foreach (Transaction t in DataBase.Transactions().Where(z => z.EnterpriseGuid == Guid))
+            amount += t.Amount;
+        return amount;
+    }
+}
