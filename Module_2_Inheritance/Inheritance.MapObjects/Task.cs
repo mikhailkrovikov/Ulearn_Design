500c9f2d7fdb453b0c46996ff4c1cb10bb4363aa
mkrovikov <mikhailkrovikov@gmail.com>
2026-07-31 19:32:02 +0300

    map object task

--- Module_2_Inheritance/Inheritance.MapObjects/Task.cs
+++ Module_2_Inheritance/Inheritance.MapObjects/Task.cs
@@ -1,80 +1,108 @@
 namespace Inheritance.MapObjects;
 
-public class Dwelling
+public interface IAssignable
 {
-	public int Owner { get; set; }
+    public int Owner { get; set; }
+}
+public interface ICollecatble
+{
+    public Treasure Treasure { get; set; }
+}
+public interface IFightable
+{
+    public Army Army { get; set; }
 }
 
-public class Mine
+public class Dwelling : IAssignable
 {
-	public int Owner { get; set; }
-	public Army Army { get; set; }
-	public Treasure Treasure { get; set; }
+    public int Owner { get; set; }
 }
 
-public class Creeps
+public class Mine : IAssignable, IFightable, ICollecatble
 {
-	public Army Army { get; set; }
-	public Treasure Treasure { get; set; }
+    public int Owner { get; set; }
+    public Army Army { get; set; }
+    public Treasure Treasure { get; set; }
 }
 
-public class Wolves
+public class Creeps : IFightable, ICollecatble
 {
-	public Army Army { get; set; }
+    public Army Army { get; set; }
+    public Treasure Treasure { get; set; }
 }
 
-public class ResourcePile
+public class Wolves : IFightable
 {
-	public Treasure Treasure { get; set; }
+    public Army Army { get; set; }
+}
+
+public class ResourcePile : ICollecatble
+{
+    public Treasure Treasure { get; set; }
 }
 
 public static class Interaction
 {
-	public static void Make(Player player, object mapObject)
-	{
-		//Здесь и далее используйте следующий сокращенный синтаксис преобразования типа
-		if (mapObject is Dwelling dwellingObj)
-		{
-			//Он более короткий и позволяет не производить множественных преобразований таких, как
-			//((Dwelling)mapObject).Owner = player.Id;
+    public static void Make(Player player, object mapObject)
+    {
+        if (mapObject is IAssignable assignable and IFightable fightable1 and ICollecatble collecatble2)
+        {
+            HandleFightAndAssignAndCollect(player, assignable, fightable1, collecatble2);
+            return;
+        }
+        if (mapObject is IFightable fightable2 and ICollecatble collecatble1)
+        {
+            HandleFightAndCollect(player, fightable2, collecatble1);
+            return;
+        }
+        if (mapObject is IFightable fightable)
+            HandleFight(player, fightable);
+        if (mapObject is ICollecatble collecatble)
+        {
+            HandleCollecatble(player, collecatble);
+            return;
+        }
+        if (mapObject is IAssignable dwellingObj)
+        {
+            HandleAssign(player, dwellingObj);
+            return;
+        }
+    }
 
-			//а сразу обращаться к объекту, если он является каким-то классом.
-			dwellingObj.Owner = player.Id;
+    private static void HandleFight(Player player, IFightable fightable)
+    {
+        if (!player.CanBeat(fightable.Army))
+            player.Die();
+    }
 
-			return;
-		}
+    private static void HandleAssign(Player player, IAssignable assignable)
+    {
+        assignable.Owner = player.Id;
+    }
 
-		//Перед выполнение задания потренируйтесь и замените неправильное использование is ниже
-		if (mapObject is Mine)
-		{
-			if (player.CanBeat(((Mine)mapObject).Army))
-			{
-				((Mine)mapObject).Owner = player.Id;
-				player.Consume(((Mine)mapObject).Treasure);
-			}
-			else player.Die();
-			return;
-		}
+    private static void HandleCollecatble(Player player, ICollecatble collecatble)
+    {
+        player.Consume(collecatble.Treasure);
+    }
 
-		if (mapObject is Creeps)
-		{
-			if (player.CanBeat(((Creeps)mapObject).Army))
-				player.Consume(((Creeps)mapObject).Treasure);
-			else
-				player.Die();
-			return;
-		}
+    private static void HandleFightAndCollect(Player player, IFightable fightable, ICollecatble collecatble)
+    {
+        if (player.CanBeat(fightable.Army))
+            player.Consume(collecatble.Treasure);
+        else
+            player.Die();
+    }
 
-		if (mapObject is ResourcePile)
-		{
-			player.Consume(((ResourcePile)mapObject).Treasure);
-			return;
-		}
-
-		if (mapObject is Wolves)
-		{
-			if (!player.CanBeat(((Wolves)mapObject).Army))
-				player.Die();
-		}
-	}
+    private static void HandleFightAndAssignAndCollect(Player player,
+        IAssignable assignable,
+        IFightable fightable,
+        ICollecatble collecatble)
+    {
+        if (player.CanBeat(fightable.Army))
+        {
+            assignable.Owner = player.Id;
+            player.Consume(collecatble.Treasure);
+        }
+        else player.Die();
+    }
 }
