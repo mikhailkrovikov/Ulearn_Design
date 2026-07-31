4de555a488af5b6558e4297536dd0c1fbb0b4ca7
mkrovikov <mikhailkrovikov@gmail.com>
2026-07-31 18:29:20 +0300

    weights task

---
+++ Module_1_Incapsulation/Incapsulation.Weights/Indexer.cs
@@ -0,0 +1,36 @@
+﻿namespace Incapsulation.Weights;
+
+public class Indexer
+{
+    private readonly double[] _array;
+    private readonly int _start;
+    private readonly int _length;
+
+    public double this[int index]
+    {
+        get
+        {
+            if (index >= _array.Length || index < 0 || index >= _length)
+                throw new IndexOutOfRangeException();
+            double value = _array[_start + index];
+            return value;
+        }
+        set
+        {
+            if (index >= _array.Length || index < 0 || index >= _length)
+                throw new IndexOutOfRangeException();
+            _array[_start + index] = value;
+        }
+    }
+
+    public Indexer(double[] array, int start, int length)
+    {
+        if (length < 0 || start < 0 || start + length > array.Length)
+            throw new ArgumentException($"Range is invalid with start: {start}, lenth:{length}");
+        _array = array;
+        _start = start;
+        _length = length;
+    }
+
+    public int Length => _length;
+}
