6f7994b6dc84c5709e90c5c1c844c428668fe751
mkrovikov <mikhailkrovikov@gmail.com>
2026-08-12 14:46:06 +0300

    add Generics proj

---
+++ Module_3_Generics/Generics.BinaryTrees/BinaryTree.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+namespace Generics.BinaryTrees;
+
+public class BinaryTree<T> : IEnumerable<T> where T : IComparable<T>
+{
+    public T Value { get; set; }
+    public List<T> List { get; set; } = new List<T>();
+    public void Add(T value)
+    {
+        List.Add(value);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public BinaryTree<T> Left { get; set; }
+    public BinaryTree<T> Right { get; set; }
+
+}
+
+public class BinaryTree : BinaryTree<int>
+{
+    public static new BinaryTree Create(params int[] values)
+    {
+        var tree = new BinaryTree();
+        foreach (var value in values)
+        {
+            tree.Add(value);
+        }
+        return tree;
+    }
+}
