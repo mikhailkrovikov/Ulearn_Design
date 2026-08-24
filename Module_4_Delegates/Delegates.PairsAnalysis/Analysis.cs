c30b6ed1af205a461fcbaab78e210765f9ca78eb
mkrovikov <mikhailkrovikov@gmail.com>
2026-08-24 20:46:49 +0300

    pairs task

--- Module_4_Delegates/Delegates.PairsAnalysis/Analysis.cs
+++ Module_4_Delegates/Delegates.PairsAnalysis/Analysis.cs
@@ -2,13 +2,52 @@
 
 public static class Analysis
 {
-	public static int FindMaxPeriodIndex(params DateTime[] data)
-	{
-		return new MaxPauseFinder().Analyze(data);
-	}
+    public static int FindMaxPeriodIndex(params DateTime[] data)
+    {
+        return data.Pairs()
+            .Select(p => (p.Item2 - p.Item1).TotalSeconds)
+            .MaxIndex();
+    }
 
-	public static double FindAverageRelativeDifference(params double[] data)
-	{
-		return new AverageDifferenceFinder().Analyze(data);
-	}
+    public static double FindAverageRelativeDifference(params double[] data)
+    {
+        return data.Pairs()
+            .Average(x => (x.Item2 - x.Item1) / x.Item1);
+    }
+}
+
+public static class MyExtentions
+{
+    public static IEnumerable<Tuple<T, T>> Pairs<T>(this IEnumerable<T> collection)
+    {
+        using var e = collection.GetEnumerator();
+        if (!e.MoveNext())
+            yield break;
+        var current = e.Current;
+        while (e.MoveNext())
+        {
+            var next = e.Current;
+            yield return Tuple.Create(current, next);
+            current = next;
+        }
+    }
+
+    public static int MaxIndex<T>(this IEnumerable<T> collection) where T : IComparable<T>
+    {
+        var maxValue = default(T);
+        var maxIndex = -1;
+        var i = 0;
+        foreach (var item in collection)
+        {
+            if (item.CompareTo(maxValue) >= 0)
+            {
+                maxValue = item;
+                maxIndex = i;
+            }
+            i++;
+        }
+        if (maxIndex == -1)
+            throw new InvalidOperationException();
+        return maxIndex;
+    }
 }
