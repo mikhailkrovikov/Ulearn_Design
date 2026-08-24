90cdba4c9ca37ceae01ff6ad1ff53e634f28a918
mkrovikov <mikhailkrovikov@gmail.com>
2026-08-24 23:04:42 +0300

    build

--- Module_5_Reflection/Reflection.Randomness/Generator.cs
+++ Module_5_Reflection/Reflection.Randomness/Generator.cs
@@ -1,1 +1,20 @@
 ﻿namespace Reflection.Randomness;
+
+public class FromDistributionAttribute : Attribute
+{
+    public Type DistributionType { get; }
+    public object[] DistributionParameters { get; }
+    public FromDistributionAttribute(Type distributionType, params object[] distributionParameters)
+    {
+        DistributionType = distributionType;
+        DistributionParameters = distributionParameters;
+    }
+}
+
+public class Generator<T>
+{
+    public T Generate(Random rnd)
+    {
+        throw new NotImplementedException();
+    }
+}
