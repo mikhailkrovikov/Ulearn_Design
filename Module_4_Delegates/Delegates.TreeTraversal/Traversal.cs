d112cfee68b8518aa4930a4924bd9e8f5757adaa
mkrovikov <mikhailkrovikov@gmail.com>
2026-08-26 19:20:59 +0300

    traversal task

--- Module_4_Delegates/Delegates.TreeTraversal/Traversal.cs
+++ Module_4_Delegates/Delegates.TreeTraversal/Traversal.cs
@@ -2,18 +2,38 @@ namespace Delegates.TreeTraversal;
 
 public static class Traversal
 {
-	public static IEnumerable<Product> GetProducts(ProductCategory root)
-	{
-		throw new NotImplementedException();
-	}
+    public static IEnumerable<TResult> TraversalTree<TNode, TResult>(
+        TNode node,
+        Func<TNode, IEnumerable<TNode>> getChildren,
+        Func<TNode, IEnumerable<TResult>> getValues)
+    {
+        foreach (var value in getValues(node))
+            yield return value;
+        foreach (var child in getChildren(node))
+            foreach (var value in TraversalTree(child, getChildren, getValues))
+                yield return value;
+    }
 
-	public static IEnumerable<Job> GetEndJobs(Job root)
-	{
-		throw new NotImplementedException();
-	}
+    public static IEnumerable<Product> GetProducts(ProductCategory root)
+    {
+        return TraversalTree(root,
+            p => p.Categories,
+            p => p.Products);
+    }
 
-	public static IEnumerable<T> GetBinaryTreeValues<T>(BinaryTree<T> root)
-	{
-		throw new NotImplementedException();
-	}
+    public static IEnumerable<Job> GetEndJobs(Job root)
+    {
+        return TraversalTree(root,
+            j => j.Subjobs,
+            j => j.Subjobs.Count == 0 ? [j] : Array.Empty<Job>());
+    }
+
+    public static IEnumerable<T> GetBinaryTreeValues<T>(BinaryTree<T> root)
+    {
+        return TraversalTree(root,
+            n => new[] { n.Left, n.Right }.Where(c => c != null)!,
+            n => n.Left == null && n.Right == null
+            ? [n.Value]
+            : Array.Empty<T>());
+    }
 }
