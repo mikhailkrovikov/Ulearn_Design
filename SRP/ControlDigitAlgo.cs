037b27d07cd5f6b19d7736b27e17a78989fad0e6
mkrovikov <mikhailkrovikov@gmail.com>
2026-09-15 23:41:43 +0300

    srp task

--- SRP/ControlDigitAlgo.cs
+++ SRP/ControlDigitAlgo.cs
@@ -2,39 +2,68 @@ namespace SRP.ControlDigit;
 
 public static class Extensions
 {
-	// Вспомогательные методы-расширения поместите в этот класс.
-	// Они должны быть понятны и потенциально полезны вне контекста задачи расчета контрольных разрядов.
+    public static IEnumerable<int> GetDigitsFromRight(this long number)
+    {
+        do
+        {
+            yield return (int)(number % 10);
+            number /= 10;
+        }
+        while (number > 0);
+    }
+
+    public static int Sum(this IEnumerable<int> source, Func<int, int, int> selector)
+    {
+        int sum = 0;
+        int index = 0;
+        foreach (var item in source)
+        {
+            sum += selector(item, index);
+            index++;
+        }
+        return sum;
+    }
+
+    public static int GetReminder(this int number, int divisor)
+    {
+        return (divisor - number % divisor) % divisor;
+    }
+
+    public static char ToChar(this int number, int selector)
+    {
+        return number == selector ? 'X' : (char)('0' + number);
+    }
 }
 
 public static class ControlDigitAlgo
 {
-	public static int Upc(long number)
-	{
-		int sum = 0;
-		int factor = 3;
-		do
-		{
-			int digit = (int)(number % 10);
-			sum += factor * digit;
-			factor = 4 - factor;
-			number /= 10;
+    public static int Upc(long number)
+    {
+        return number
+            .GetDigitsFromRight()
+            .Take(11)
+            .Sum((digit, index) => digit * (index % 2 == 0 ? 3 : 1))
+            .GetReminder(10);
+    }
 
-		}
-		while (number > 0);
+    public static char Isbn10(long number)
+    {
+        return number
+            .GetDigitsFromRight()
+            .Sum((digit, index) => digit * (index + 2))
+            .GetReminder(11)
+            .ToChar(10);
+    }
 
-		int result = sum % 10;
-		if (result != 0)
-			result = 10 - result;
-		return result;
-	}
-
-	public static int Isbn10(long number)
-	{
-		throw new NotImplementedException();
-	}
-
-	public static int Luhn(long number)
-	{
-		throw new NotImplementedException();
-	}
+    public static int Luhn(long number)
+    {
+        return number.GetDigitsFromRight().Sum((digit, index) =>
+        {
+            if (index % 2 == 0)
+            {
+                digit *= 2;
+                if (digit > 9) digit -= 9;
+            } return digit;
+        }).GetReminder(10);
+    }
 }
