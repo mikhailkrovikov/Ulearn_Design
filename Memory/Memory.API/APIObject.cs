5dff0e7deecb99344edb3bb3baf72512d5a94bdd
mkrovikov <mikhailkrovikov@gmail.com>
2026-09-16 18:25:24 +0300

    memory api task

--- Memory/Memory.API/APIObject.cs
+++ Memory/Memory.API/APIObject.cs
@@ -1,1 +1,32 @@
 ﻿namespace Memory.API;
+
+public class APIObject : IDisposable
+{
+    private bool isDisposed = false;
+    private readonly int id;
+    public APIObject(int id)
+    {
+        this.id = id;
+        MagicAPI.Allocate(id);
+    }
+
+    ~APIObject()
+    {
+        Dispose(false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!isDisposed)
+        {
+            MagicAPI.Free(id);
+            isDisposed = true;
+        }
+    }
+}
